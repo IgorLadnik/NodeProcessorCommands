d6a8b12bf82f9d8e903f3a36273b6421b19ee045
ladni <ladnik@hotmail.com>
2020-06-16 10:58:49 +0300

    RebbitMQ support moved out of processor

---
+++ src/commands/cmdRabbitMQConsumer-1.ts
@@ -0,0 +1,18 @@
+export async function command(args: any, p: any, message: any): Promise<boolean> {
+    const thisCommandName = 'cmdRabbitMQConsumer';
+    let logger = p.getLogger();
+
+    /*await*/ logger.log(`Command ${thisCommandName} started  args: ${JSON.stringify(args)} ${!message.isEmpty ? `, message: ${message}` : ''}`);
+
+    const Config = (await import(`${p.workingDir}/config`)).Config;
+    const _ = await import(`${p.stdImportDir}/lodash`);
+    const Consumer = (await import(`${p.stdImportDir}/rabbitmq-provider/consumer`)).Consumer;
+    
+    const consumer = await Consumer.createConsumer(Config.messageBroker, logger,
+                            (thisConsumer: any, msg: any) => p.getCommandFromQueueMessageAndExecute(msg));
+    p.setResource('consumer', consumer);
+
+    /*await*/ logger.log(`Command ${thisCommandName} ended`);
+    
+    return true;
+}
---
+++ src/commands/cmdRabbitMQPublisher-1.ts
@@ -0,0 +1,31 @@
+export async function command(args: any, p: any, message: any): Promise<boolean> {
+    const thisCommandName = 'cmdRabbitMQPublisher';
+    let logger = p.getLogger();
+
+    /*await*/ logger.log(`Command ${thisCommandName} started  args: ${JSON.stringify(args)} ${!message.isEmpty ? `, message: ${message}` : ''}`);
+
+    const Command = require(`${p.workingDir}/models/command`).Command;
+    const Config = (await import(`${p.workingDir}/config`)).Config;
+    const _ = await import(`${p.stdImportDir}/lodash`);
+    const Publisher = (await import(`${p.stdImportDir}/rabbitmq-provider/publisher`)).Publisher;
+    
+    const publisher = await Publisher.createPublisher(Config.messageBroker, logger);
+    p.setResource('publisher', publisher);
+
+    setInterval(async () =>
+            await publisher.publishAsync(
+                new Command('cmdFirstFetch', {a: 'aaa', n: 1}),
+                new Command('cmdFirstFetch', {a: 'qqq', n: 1})),
+        3000);
+
+    setInterval(async () =>
+            await publisher.publishAsync(
+                new Command('cmdTestP', {order: 1}),
+                new Command('cmdTestP', {order: 2}),
+                new Command('cmdTestP', {order: 3})),
+        1370);
+
+    /*await*/ logger.log(`Command ${thisCommandName} ended`);
+    
+    return true;
+}
