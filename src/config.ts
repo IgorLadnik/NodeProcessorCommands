03fc22ef5227dad6548fb2a5b4a74d0f58476adf
ladni <ladnik@hotmail.com>
2020-06-19 13:01:29 +0300

    isRunStandAlone = true

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export class Config {
 
-    static isRunStandAlone = false; // false to run with RabbitMQ and SQL Server
+    static isRunStandAlone = true; // false to run with RabbitMQ and SQL Server
 
     // PROCESSOR CONFIGURATION
 
